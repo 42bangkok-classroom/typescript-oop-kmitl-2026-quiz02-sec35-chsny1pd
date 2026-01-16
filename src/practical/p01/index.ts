2d854eda8ae3f605ee178b045404a95b76f60ea4
Setsin Chutsanayothin <68011112@kmitl.ac.th>
2026-01-16 10:16:16 +0700

    5

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,35 +1,34 @@
-//export function getEdgePosts() {}
-// import * as https from 'https'; // 1. Import module สำหรับเรียกใช้ HTTP
 
-// function getEdgePosts(): void {
-//   const url = "https://jsonplaceholder.typicode.com/posts"; // ตัวอย่าง URL สำหรับทดสอบ
+import * as https from 'https'; // 1. Import module สำหรับเรียกใช้ HTTP
 
-//   // 2. ใช้ https.get เพื่อส่ง Request ไปยัง Server
-//   https.get(url, (res) => {
-//     let data = "";
+export function getEdgePosts(): void {
+  const url = "https://jsonplaceholder.typicode.com/posts"; // ตัวอย่าง URL สำหรับทดสอบ
 
-//     // 3. รอรับข้อมูล (ข้อมูลจะค่อยๆ ทยอยส่งมาเป็น 'chunks')
-//     res.on("data", (chunk) => {
-//       data += chunk;
-//     });
+  // 2. ใช้ https.get เพื่อส่ง Request ไปยัง Server
+  https.get(url, (res) => {
+    let data = "";
 
-//     // 4. เมื่อได้รับข้อมูลจนครบ (End of response)
-//     res.on("end", () => {
-//       try {
-//         // แปลงข้อความ String ให้เป็น JSON Object เพื่อให้แสดงผลสวยงาม
-//         const jsonResponse = JSON.parse(data);
-//         console.log(jsonResponse);
-//       } catch (err) {
-//         console.error("Error parsing JSON");
-//       }
-//     });
+    // 3. รอรับข้อมูล (ข้อมูลจะค่อยๆ ทยอยส่งมาเป็น 'chunks')
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
 
-//   }).on("error", (err) => {
-//     // จัดการกรณีที่เกิดปัญหาด้านเครือข่าย
-//     console.log(`Error: ${err.message}`);
-//   });
-// }
+    // 4. เมื่อได้รับข้อมูลจนครบ (End of response)
+    res.on("end", () => {
+      try {
+        // แปลงข้อความ String ให้เป็น JSON Object เพื่อให้แสดงผลสวยงาม
+        const jsonResponse = JSON.parse(data);
+        console.log(jsonResponse);
+      } catch (err) {
+        console.error("Error parsing JSON");
+      }
+    });
 
-// // ---- เรียกใช้งานฟังก์ชัน ----
-// getEdgePosts();
-console.log('dasdad')
+  }).on("error", (err) => {
+    // จัดการกรณีที่เกิดปัญหาด้านเครือข่าย
+    console.log(`Error: ${err.message}`);
+  });
+}
+
+// ---- เรียกใช้งานฟังก์ชัน ----
+getEdgePosts();
