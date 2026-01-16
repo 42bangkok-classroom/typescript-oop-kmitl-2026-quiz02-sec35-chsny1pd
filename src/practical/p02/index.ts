8e0ff5b600397cc3fea41133631d0be5621137e2
Setsin Chutsanayothin <68011112@kmitl.ac.th>
2026-01-16 11:22:30 +0700

    12

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,26 @@
-export function getPostsByUser() {}
+import axios from 'axios';
+
+export const getPostsByUser = async(userId: number) : Promise<any> => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const data = response.data;
+
+    // ใช้ .map() เพื่อสร้าง Array ใหม่ที่มีเฉพาะ 'id' และ 'title'
+    const filteredData = data.map((item: { id: any; title: any; }) => ({
+      id: item.id,
+      title: item.title
+    }));
+    const firstItem = filteredData[0]; 
+    const lastItem = filteredData[filteredData.length - 1];
+
+    console.log(firstItem);
+    console.log(lastItem);
+
+    const result = [firstItem, lastItem];
+    return result.filter(cart => cart.userId === userId);
+    } catch (error) {
+    console.error("เกิดข้อผิดพลาด:", error);
+  }
+}
+
+getPostsByUser(1);
