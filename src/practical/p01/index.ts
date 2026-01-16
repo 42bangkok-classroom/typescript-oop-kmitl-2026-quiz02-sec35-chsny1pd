1dfac90cf1985b606f90ea2cc76a45d2392937b8
Setsin Chutsanayothin <68011112@kmitl.ac.th>
2026-01-16 11:17:50 +0700

    11

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -10,61 +10,60 @@
 //     }
 // };
 
-// import axios from 'axios';
-
-// export const getEdgePosts = async() : Promise<any[]> => {
-//   try {
-//     const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-//     const data = response.data;
-
-//     // ใช้ .map() เพื่อสร้าง Array ใหม่ที่มีเฉพาะ 'id' และ 'title'
-//     const filteredData = data.map((item: { id: any; title: any; }) => ({
-//       id: item.id,
-//       title: item.title
-//     }));
-
-//     console.log(filteredData);
-//     // ผลลัพธ์:
-//     // [
-//     //   { id: 1, title: 'delectus aut autem' },
-//     // L  { id: 2, title: 'quis ut nam facilis et officia qui' }
-//     // ]
-//     return response.data;
-//   } catch (error) {
-//     console.error("เกิดข้อผิดพลาด:", error);
-//   }
-// }
-
-// getEdgePosts();
-
 import axios from 'axios';
-//export getEdgePosts()
 
-const url = 'https://jsonplaceholder.typicode.com/posts'; 
-
-axios.get(url, {
-    params: {
-    }
-})
-.then(response => {
+export const getEdgePosts = async() : Promise<any> => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
     const data = response.data;
-   
-const allData = response.data;
 
+    // ใช้ .map() เพื่อสร้าง Array ใหม่ที่มีเฉพาะ 'id' และ 'title'
+    const filteredData = data.map((item: { id: any; title: any; }) => ({
+      id: item.id,
+      title: item.title
+    }));
+    const firstItem = filteredData[0]; 
+    const lastItem = filteredData[filteredData.length - 1];
 
-const getEdgePosts = allData.map((item: { id: any; title: any; }) => ({
-    id: item.id,
-    title: item.title
-}));
+    console.log(firstItem);
+    console.log(lastItem);
 
-const firstItem = getEdgePosts[0]; 
-const lastItem = getEdgePosts[getEdgePosts.length - 1]; 
-
-console.log(firstItem);
-console.log(lastItem);
-
-const result = [firstItem, lastItem];
-})
-.catch(error => {
+    const result = [firstItem, lastItem];
+    return result;
+    } catch (error) {
     console.error("เกิดข้อผิดพลาด:", error);
-});
+  }
+}
+
+getEdgePosts();
+
+// import axios from 'axios';
+// //export getEdgePosts()
+// const url = 'https://jsonplaceholder.typicode.com/posts'; 
+
+// axios.get(url, {
+//     params: {
+//     }
+// })
+// .then(response => {
+//     const data = response.data;
+   
+// const allData = response.data;
+
+
+// const getEdgePosts = allData.map((item: { id: any; title: any; }) => ({
+//     id: item.id,
+//     title: item.title
+// }));
+
+// const firstItem = getEdgePosts[0]; 
+// const lastItem = getEdgePosts[getEdgePosts.length - 1]; 
+
+// console.log(firstItem);
+// console.log(lastItem);
+
+// const result = [firstItem, lastItem];
+// })
+// .catch(error => {
+//     console.error("เกิดข้อผิดพลาด:", error);
+// });
