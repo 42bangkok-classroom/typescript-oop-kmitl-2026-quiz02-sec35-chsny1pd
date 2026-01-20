e8b6745dc14a146b6440ffb2c93919f4eac060d6
Setsin Chutsanayothin <68011112@kmitl.ac.th>
2026-01-20 16:38:35 +0700

    ilovefs

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -14,7 +14,7 @@ interface SafedComment {
 }
 
 //กำหนดฟังก์ชันแบบ Asynchronous ที่ส่งค่ากลับเป็น Array ของ SimplifiedPost หรือ undefined
-export async function safeFetchComment(commentId: number): Promise<SafedComment[]|null> {
+export async function safeFetchComment(commentId: number): Promise<SafedComment[]|null|SafedComment> {
   // ใช้ try เพื่อเริ่มการดักจับข้อผิดพลาด (Error Handling) 
   // หากการทำงานภายในบล็อกนี้มีปัญหา จะกระโดดไปที่ catch ทันที
   try {
@@ -23,7 +23,7 @@ export async function safeFetchComment(commentId: number): Promise<SafedComment[
      * <Post[]> คือการระบุ Generic เพื่อบอกว่าข้อมูลที่ตอบกลับมา (response.data) 
      * จะมีโครงสร้างเป็น Array ของวัตถุ Post
      */
-    const response = await axios.get<Comment[]>('https://jsonplaceholder.typicode.com/comments/{id}');
+    const response = await axios.get<Comment>('https://jsonplaceholder.typicode.com/comments/{id}');
     /**
      * 4. ดึงข้อมูลจริง (Body) ที่ได้จาก API มาเก็บไว้ในตัวแปร posts
      * ซึ่งตัวแปรนี้จะเป็น Array ที่บรรจุโพสต์ทั้งหมด (ปกติจะมี 100 โพสต์)
@@ -34,19 +34,13 @@ export async function safeFetchComment(commentId: number): Promise<SafedComment[
      * ถ้าข้อมูลที่ได้มาเป็น Array ว่างเปล่า (ไม่มีโพสต์เลย) 
      * ให้หยุดการทำงานและส่ง Array เปล่ากลับออกไปทันที
      */
-    if (!comments.length) return null;
+    if (!comments) return null;
 
-    // ใช้ Array method (map) เพื่อสร้างอาเรย์ใหม่ที่มีเฉพาะ id และ title
-    // หรือจะสร้าง Array ตรงๆ แล้วครอบด้วย map เพื่อดึงเฉพาะ field ที่ต้องการ
-    const filteredComments = comments.filter((comment) => comment.postId === commentId)
-
-    const mapcomments = filteredComments.map((comment) => ({
-      id: comment.id,
-      body: comment.body
-    }))
-
-    return mapcomments
-
+    return{
+      id: comments.id,
+      body: comments.body
+    }
+    
   } catch (error) {
     // จัดการ error ตามความเหมาะสม (ในที่นี้คือ return undefined หรือจะโยน error ต่อก็ได้)
     return null
