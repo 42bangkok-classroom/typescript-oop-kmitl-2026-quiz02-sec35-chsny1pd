2caa76275ef9b0f80c32ca9705a7ed06d992190d
PD <168636679+chsny1pd@users.noreply.github.com>
2026-01-21 10:50:25 +0700

    momo

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -1,5 +1,54 @@
-import axios from 'axios';
-// กำหนด Interface เพื่อหลีกเลี่ยงการใช้ 'any'
+// import axios from 'axios';
+// // กำหนด Interface เพื่อหลีกเลี่ยงการใช้ 'any'
+// interface Comment {
+//   postId: number;
+//   id: number;
+//   name: string;
+//   email: string;
+//   body: string;
+// }
+
+// interface SafedComment {
+//   id: number;
+//   body: string;
+// }
+
+// //กำหนดฟังก์ชันแบบ Asynchronous ที่ส่งค่ากลับเป็น Array ของ SimplifiedPost หรือ undefined
+// export async function safeFetchComment(commentId: number): Promise<null|SafedComment> {
+//   // ใช้ try เพื่อเริ่มการดักจับข้อผิดพลาด (Error Handling) 
+//   // หากการทำงานภายในบล็อกนี้มีปัญหา จะกระโดดไปที่ catch ทันที
+//   try {
+//     /**
+//      * 3. สั่งให้โปรแกรม "รอ" (await) การดึงข้อมูลจาก API ผ่าน axios
+//      * <Post[]> คือการระบุ Generic เพื่อบอกว่าข้อมูลที่ตอบกลับมา (response.data) 
+//      * จะมีโครงสร้างเป็น Array ของวัตถุ Post
+//      */
+//     const response = await axios.get<Comment>(`https://jsonplaceholder.typicode.com/comments/${commentId}`);
+//     /**
+//      * 4. ดึงข้อมูลจริง (Body) ที่ได้จาก API มาเก็บไว้ในตัวแปร posts
+//      * ซึ่งตัวแปรนี้จะเป็น Array ที่บรรจุโพสต์ทั้งหมด (ปกติจะมี 100 โพสต์)
+//      */
+//     const comments = response.data;
+//     /**
+//      * 5. ตรวจสอบเงื่อนไขเผื่อไว้ (Guard Clause):
+//      * ถ้าข้อมูลที่ได้มาเป็น Array ว่างเปล่า (ไม่มีโพสต์เลย) 
+//      * ให้หยุดการทำงานและส่ง Array เปล่ากลับออกไปทันที
+//      */
+//     if (!comments) return null;
+
+//     return{
+//       id: comments.id,
+//       body: comments.body
+//     }
+
+//   } catch (error) {
+//     // จัดการ error ตามความเหมาะสม (ในที่นี้คือ return undefined หรือจะโยน error ต่อก็ได้)
+//     return null
+//   }
+// }
+
+import axios from 'axios'
+
 interface Comment {
   postId: number;
   id: number;
@@ -13,36 +62,17 @@ interface SafedComment {
   body: string;
 }
 
-//กำหนดฟังก์ชันแบบ Asynchronous ที่ส่งค่ากลับเป็น Array ของ SimplifiedPost หรือ undefined
-export async function safeFetchComment(commentId: number): Promise<null|SafedComment> {
-  // ใช้ try เพื่อเริ่มการดักจับข้อผิดพลาด (Error Handling) 
-  // หากการทำงานภายในบล็อกนี้มีปัญหา จะกระโดดไปที่ catch ทันที
-  try {
-    /**
-     * 3. สั่งให้โปรแกรม "รอ" (await) การดึงข้อมูลจาก API ผ่าน axios
-     * <Post[]> คือการระบุ Generic เพื่อบอกว่าข้อมูลที่ตอบกลับมา (response.data) 
-     * จะมีโครงสร้างเป็น Array ของวัตถุ Post
-     */
-    const response = await axios.get<Comment>(`https://jsonplaceholder.typicode.com/comments/${commentId}`);
-    /**
-     * 4. ดึงข้อมูลจริง (Body) ที่ได้จาก API มาเก็บไว้ในตัวแปร posts
-     * ซึ่งตัวแปรนี้จะเป็น Array ที่บรรจุโพสต์ทั้งหมด (ปกติจะมี 100 โพสต์)
-     */
-    const comments = response.data;
-    /**
-     * 5. ตรวจสอบเงื่อนไขเผื่อไว้ (Guard Clause):
-     * ถ้าข้อมูลที่ได้มาเป็น Array ว่างเปล่า (ไม่มีโพสต์เลย) 
-     * ให้หยุดการทำงานและส่ง Array เปล่ากลับออกไปทันที
-     */
-    if (!comments) return null;
-
+export async function safeFetchComment(commentId:number):Promise<SafedComment|null> {
+  try{
+    const response = await axios.get<Comment>(`https://jsonplaceholder.typicode.com/comments/{id}`)
+    const comments = response.data
+    if(!comments) return null;
     return{
       id: comments.id,
       body: comments.body
     }
-
-  } catch (error) {
-    // จัดการ error ตามความเหมาะสม (ในที่นี้คือ return undefined หรือจะโยน error ต่อก็ได้)
+  }
+  catch(error){
     return null
   }
 }
