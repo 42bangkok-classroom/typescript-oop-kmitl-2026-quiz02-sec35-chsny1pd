756cb423a74def3fb7bac57245eb4c3ae92e991e
PD <168636679+chsny1pd@users.noreply.github.com>
2026-01-21 10:13:21 +0700

    p4

--- src/practical/p03/index.ts
+++ src/practical/p03/index.ts
@@ -1,60 +1,100 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-// กำหนด Interface เพื่อหลีกเลี่ยงการใช้ 'any'
+// // กำหนด Interface เพื่อหลีกเลี่ยงการใช้ 'any'
+// interface Post {
+//   id: number;
+//   title: string;
+// }
+
+// interface Comment {
+//   postId: number;
+//   id: number;
+// }
+
+// interface PostWithCount {
+//   postId: number;
+//   title: string;
+//   totalComments: number;
+// }
+
+// //กำหนดฟังก์ชันแบบ Asynchronous ที่ส่งค่ากลับเป็น Array ของ SimplifiedPost หรือ undefined
+// export async function mapPostWithCommentCount(): Promise<PostWithCount[]> {
+//   // ใช้ try เพื่อเริ่มการดักจับข้อผิดพลาด (Error Handling) 
+//   // หากการทำงานภายในบล็อกนี้มีปัญหา จะกระโดดไปที่ catch ทันที
+//   try {
+//     /**
+//      * 3. สั่งให้โปรแกรม "รอ" (await) การดึงข้อมูลจาก API ผ่าน axios
+//      * <Post[]> <Comment[]> คือการระบุ Generic เพื่อบอกว่าข้อมูลที่ตอบกลับมา (postres.data) , (commentres.data)
+//      * จะมีโครงสร้างเป็น Array ของวัตถุ Post
+//      */
+//     const postres = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+//     const commentres = await axios.get<Comment[]>('https://jsonplaceholder.typicode.com/comments');
+//     /**
+//      * 4. ดึงข้อมูลจริง (Body) ที่ได้จาก API มาเก็บไว้ในตัวแปร posts , comments
+//      * ซึ่งตัวแปรนี้จะเป็น Array ที่บรรจุโพสต์ทั้งหมด (ปกติจะมี 100 โพสต์)
+//      */
+//     const posts = postres.data;
+//     const comments = commentres.data;
+//     /**
+//      * 5. ตรวจสอบเงื่อนไขเผื่อไว้ (Guard Clause):
+//      * ถ้าข้อมูลที่ได้มาเป็น Array ว่างเปล่า (ไม่มีโพสต์เลย) 
+//      * ให้หยุดการทำงานและส่ง Array เปล่ากลับออกไปทันที
+//      */
+//     if (!posts.length) return [];
+
+//     // คืนค่าที่โจทย์ต้องการ
+//     return posts.map((post) => { 
+//     // ใช้ Array method (map) เพื่อสร้างอาเรย์ใหม่ของ comment ที่มี postId เหมือนกัน จะได้นับจำนวนง่าย
+//     const CommentedPosts = comments.filter((comment) => comment.postId === post.id)
+//     return {
+//      postId: post.id,
+//      title: post.title,
+//      totalComments: CommentedPosts.length
+//     };
+//     });
+  
+//   } catch (error) {
+//     // จัดการ error ตามความเหมาะสม (ในที่นี้คือ return undefined หรือจะโยน error ต่อก็ได้)
+//     return [];
+//   }
+// }
+
+import axios from 'axios'
+
 interface Post {
-  id: number;
-  title: string;
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
 }
 
 interface Comment {
-  postId: number;
-  id: number;
+  postId: number,
+  id: number,
 }
 
-interface PostWithCount {
-  postId: number;
-  title: string;
+interface PostWithComment {
+  postId: number,
+  title: string,
   totalComments: number;
 }
 
-//กำหนดฟังก์ชันแบบ Asynchronous ที่ส่งค่ากลับเป็น Array ของ SimplifiedPost หรือ undefined
-export async function mapPostWithCommentCount(): Promise<PostWithCount[]> {
-  // ใช้ try เพื่อเริ่มการดักจับข้อผิดพลาด (Error Handling) 
-  // หากการทำงานภายในบล็อกนี้มีปัญหา จะกระโดดไปที่ catch ทันที
-  try {
-    /**
-     * 3. สั่งให้โปรแกรม "รอ" (await) การดึงข้อมูลจาก API ผ่าน axios
-     * <Post[]> <Comment[]> คือการระบุ Generic เพื่อบอกว่าข้อมูลที่ตอบกลับมา (postres.data) , (commentres.data)
-     * จะมีโครงสร้างเป็น Array ของวัตถุ Post
-     */
-    const postres = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
-    const commentres = await axios.get<Comment[]>('https://jsonplaceholder.typicode.com/comments');
-    /**
-     * 4. ดึงข้อมูลจริง (Body) ที่ได้จาก API มาเก็บไว้ในตัวแปร posts , comments
-     * ซึ่งตัวแปรนี้จะเป็น Array ที่บรรจุโพสต์ทั้งหมด (ปกติจะมี 100 โพสต์)
-     */
-    const posts = postres.data;
-    const comments = commentres.data;
-    /**
-     * 5. ตรวจสอบเงื่อนไขเผื่อไว้ (Guard Clause):
-     * ถ้าข้อมูลที่ได้มาเป็น Array ว่างเปล่า (ไม่มีโพสต์เลย) 
-     * ให้หยุดการทำงานและส่ง Array เปล่ากลับออกไปทันที
-     */
-    if (!posts.length) return [];
-
-    // คืนค่าที่โจทย์ต้องการ
-    return posts.map((post) => { 
-    // ใช้ Array method (map) เพื่อสร้างอาเรย์ใหม่ของ comment ที่มี postId เหมือนกัน จะได้นับจำนวนง่าย
-    const CommentedPosts = comments.filter((comment) => comment.postId === post.id)
-    return {
-     postId: post.id,
-     title: post.title,
-     totalComments: CommentedPosts.length
-    };
-    });
-  
-  } catch (error) {
-    // จัดการ error ตามความเหมาะสม (ในที่นี้คือ return undefined หรือจะโยน error ต่อก็ได้)
-    return [];
+export async function mapPostWithCommentCount():Promise<PostWithComment[]> {
+  try{
+    const response1 = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+    const response2 = await axios.get<Comment[]>('https://jsonplaceholder.typicode.com/comments')
+    const comments = response2.data
+    const posts = response1.data
+    if(!posts.length) return[]
+    const mappost = posts.map((p)=>{
+      const countcomment = comments.filter((f)=> f.postId==p.id)
+      return {
+        postId:p.id,
+        title:p.title,
+        totalComments:countcomment.length }})
+    return mappost
+  }
+  catch(error){
+    return[]
   }
 }
